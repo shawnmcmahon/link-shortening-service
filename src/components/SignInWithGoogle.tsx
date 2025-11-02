09d27cc0d23d7b3f286224abeb497a7f60f1c3eb
shawnmcmahon <shawnmcmahon17@gmail.com>
2025-11-02 16:09:29 -0700

    Resolve vercel build warnings

--- src/components/SignInWithGoogle.tsx
+++ src/components/SignInWithGoogle.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useAuth } from '../lib/hooks/useAuth';
 
 export default function SignInWithGoogle() {
@@ -11,7 +10,7 @@ export default function SignInWithGoogle() {
       onClick={signInWithGoogle}
       className="flex items-center justify-center bg-white text-gray-700 font-semibold py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-100 transition duration-300 ease-in-out"
     >
-      <Image 
+      <img 
         src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" 
         alt="Google logo" 
         width={24}
